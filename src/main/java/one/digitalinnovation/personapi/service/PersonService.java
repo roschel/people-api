f452c5e97b72292a9a02bff1dfea6fc046b0b469
roschel <joaomroschel@gmail.com>
2021-11-16 22:45:52 -0300

    findall

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -7,7 +7,9 @@ import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -30,5 +32,10 @@ public class PersonService {
                 .builder()
                 .message("Created person with id " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 }
